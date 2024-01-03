73d2b0525780803e919f8671b21c04207b586f5c
salamat2008 <46232788+salamat2008@users.noreply.github.com>
2024-01-04 06:51:31 +0900

    _

--- tests/test_MColor_list_Widget.py
+++ tests/test_MColor_list_Widget.py
@@ -26,69 +26,62 @@ class TestMColor_list_widgetWidget(unittest.TestCase):
             QApplication()
         except RuntimeError:
             pass
+        self.widget = MColor_list_widgetWidget()
     
     def test_edit_current_item_positive(self):
-        widget = MColor_list_widgetWidget()
         item = QListWidgetItem()
-        item.setData(widget.ColorRole, QColor(Qt.GlobalColor.red))
-        widget.addItem(item)
-        widget.edit_current_item()
-        updated_item = widget.currentItem()
+        item.setData(self.widget.ColorRole, QColor(Qt.GlobalColor.red))
+        self.widget.addItem(item)
+        self.widget.edit_current_item()
+        updated_item = self.widget.currentItem()
         self.assertEqual("#FF0000", updated_item.text())
     
     def test_edit_current_item_negative(self):
-        widget = MColor_list_widgetWidget()
-        widget.edit_current_item()
-        self.assertIsNone(widget.currentItem())
+        self.widget.edit_current_item()
+        self.assertIsNone(self.widget.currentItem())
     
     def test_add_color_with_color(self):
-        widget = MColor_list_widgetWidget()
-        widget.add_color(QColor(Qt.GlobalColor.red))
-        item = widget.currentItem()
+        self.widget.add_color(QColor(Qt.GlobalColor.red))
+        item = self.widget.currentItem()
         self.assertIsNotNone(item)
         self.assertEqual("#FF0000", item.text())
     
     def test_add_color_with_none(self):
-        widget = MColor_list_widgetWidget()
-        widget.add_color()
-        item = widget.currentItem()
+        self.widget.add_color()
+        item = self.widget.currentItem()
         self.assertIsNotNone(item)
     
     def test_add_color_with_invalid_color(self):
-        widget = MColor_list_widgetWidget()
         with self.assertRaises(TypeError):
             # noinspection PyTypeChecker
-            widget.add_color("invalid color")
+            self.widget.add_color("invalid color")
     
     def test_draw_icon_for_item(self):
-        widget = MColor_list_widgetWidget()
         color = QColor(Qt.GlobalColor.red)
-        icon = widget.draw_icon_for_item(color)
+        icon = self.widget.draw_icon_for_item(color)
         icon = icon.pixmap(30, mode = icon.Mode.Normal, state = icon.State.On)
         self.assertEqual(30, icon.height())
         self.assertEqual(30, icon.width())
     
     def test_get_colors(self):
-        widget = MColor_list_widgetWidget()
         expected_number_of_colors = 5
         expected_colors = tuple(QColor(color) for color in QColor.colorNames()[:expected_number_of_colors])
         for color in expected_colors:
-            widget.add_color(color)
-        actual_colors = widget.get_colors()
+            self.widget.add_color(color)
+        actual_colors = self.widget.get_colors()
         self.assertEqual(expected_number_of_colors, len(actual_colors))
         self.assertTupleEqual(expected_colors, actual_colors)
     
     def test_getitems(self):
-        widget = MColor_list_widgetWidget()
         expected_number_of_colors = 5
         expected_colors = tuple(QColor(color) for color in QColor.colorNames()[:expected_number_of_colors])
         for color in expected_colors:
-            widget.add_color(color)
-        items = widget.getitems()
+            self.widget.add_color(color)
+        items = self.widget.getitems()
         self.assertEqual(expected_number_of_colors, len(items))
         for item in items:
             self.assertIsInstance(item, QListWidgetItem)
-        self.assertTupleEqual(expected_colors, tuple(item.data(widget.ColorRole) for item in items))
+        self.assertTupleEqual(expected_colors, tuple(item.data(self.widget.ColorRole) for item in items))
 
 
 if __name__ == '__main__':
--- tests/test_MText_list_Widget.py
+++ tests/test_MText_list_Widget.py
@@ -12,71 +12,63 @@ class MText_list_WidgetTests(unittest.TestCase):
             QApplication()
         except RuntimeError:
             pass
+        self.widget = MText_list_Widget()
     
     @mock.patch.object(QInputDialog, "getText")
     def test_addItem_with_no_arguments(self, mock_gettext: mock.MagicMock):
         mock_gettext.return_value = ('Some item', True)
-        widget = MText_list_Widget()
-        widget.addItem()
-        self.assertEqual(1, widget.count())
-        item_text = widget.currentItem().text()
+        self.widget.addItem()
+        self.assertEqual(1, self.widget.count())
+        item_text = self.widget.currentItem().text()
         self.assertEqual("Some item", item_text)
     
     def test_addItem_with_string_argument(self):
-        widget = MText_list_Widget()
-        widget.addItem("Item 1")
-        self.assertEqual(1, widget.count())
-        item_text = widget.currentItem().text()
+        self.widget.addItem("Item 1")
+        self.assertEqual(1, self.widget.count())
+        item_text = self.widget.currentItem().text()
         self.assertEqual("Item 1", item_text)
     
     def test_addItem_with_listwidgetitem_argument(self):
-        widget = MText_list_Widget()
         item = QListWidgetItem("Item 1")
-        widget.addItem(item)
-        self.assertEqual(1, widget.count())
-        item_text = widget.currentItem().text()
+        self.widget.addItem(item)
+        self.assertEqual(1, self.widget.count())
+        item_text = self.widget.currentItem().text()
         self.assertEqual("Item 1", item_text)
     
     def test_add_text_with_valid_text(self):
-        widget = MText_list_Widget()
-        widget.add_text("Item 1")
-        self.assertEqual(1, widget.count())
-        item_text = widget.currentItem().text()
+        self.widget.add_text("Item 1")
+        self.assertEqual(1, self.widget.count())
+        item_text = self.widget.currentItem().text()
         self.assertEqual("Item 1", item_text)
     
     def test_add_text_with_empty_text(self):
-        widget = MText_list_Widget()
-        widget.add_text("")
-        self.assertEqual(1, widget.count())
+        self.widget.add_text("")
+        self.assertEqual(1, self.widget.count())
     
     @mock.patch.object(QInputDialog, "getText")
     def test_add_text_with_no_text(self, mock_gettext: mock.MagicMock):
         mock_gettext.return_value = ("Some item", True)
-        widget = MText_list_Widget()
-        widget.add_text()
-        self.assertEqual(1, widget.count())
-        item_text = widget.currentItem().text()
+        self.widget.add_text()
+        self.assertEqual(1, self.widget.count())
+        item_text = self.widget.currentItem().text()
         self.assertEqual("Some item", item_text)
     
     def test_add_text_with_invalid_argument(self):
-        widget = MText_list_Widget()
         with self.assertRaises(TypeError):
             # noinspection PyTypeChecker
-            widget.add_text(123)
+            self.widget.add_text(123)
     
     @mock.patch.object(QInputDialog, "getText")
     def test_edit_current_item_with_valid_text(self, mock_gettext: mock.MagicMock):
         mock_gettext.return_value = ("Edited item", True)
-        widget = MText_list_Widget()
-        widget.addItem("Item 1")
-        widget.edit_current_item()
-        self.assertEqual("Edited item", widget.currentItem().text())
+        self.widget.addItem("Item 1")
+        self.widget.edit_current_item()
+        self.assertEqual("Edited item", self.widget.currentItem().text())
     
     def test_get_texts(self):
-        widget = MText_list_Widget()
-        widget.addItem("Item 1")
-        widget.addItem("Item 2")
-        texts = widget.get_texts()
+        self.widget.addItem("Item 1")
+        self.widget.addItem("Item 2")
+        texts = self.widget.get_texts()
         self.assertEqual(2, len(texts))
         self.assertEqual("Item 1", texts[0])
         self.assertEqual("Item 2", texts[1])
