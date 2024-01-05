7779020f414ab6749154783a2f7e6f768cabc3f7
salamat2008 <46232788+salamat2008@users.noreply.github.com>
2024-01-06 08:38:30 +0900

    _

--- MWidgets/MList_Widgets/Modified_list_widget.py
+++ MWidgets/MList_Widgets/Modified_list_widget.py
@@ -1,5 +1,4 @@
 from PySide6.QtCore import QPoint, Slot
-from PySide6.QtGui import QAction
 from PySide6.QtWidgets import (
     QListWidget,
     QListWidgetItem,
@@ -15,8 +14,6 @@ class Modified_list_widget(QListWidget):
     @Slot(QPoint, name = 'show_menu')
     def show_menu(self, position: QPoint):
         menu = QMenu(self)
-        a = QAction()
-        a.connect()
         menu.addAction("Добавить", self.addItem)
         menu.addAction("Изменить", self.edit_current_item)
         menu.addAction("Вверх", self.raise_item)
