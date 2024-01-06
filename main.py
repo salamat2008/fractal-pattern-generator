993deea1d8587adde731b5534267fd2f8ad5a0a1
salamat2008 <46232788+salamat2008@users.noreply.github.com>
2024-01-06 22:40:27 +0900

    _

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from PySide6.QtWidgets import QApplication, QPushButton
+import ctypes
 
-import Lsystems.Lsystem
+lib = ctypes.CDLL(r'C:\Users\bakyt\source\repos\some_try\x64\Debug\some_try.dll')
+arr1 = ctypes.c_int * 3
+new_arr = ctypes.c_int * 3
 
-QApplication()
-QPushButton()
+size = ctypes.c_int(3)
+t_arr1 = arr1(1, 2, 3)
 
-if __name__ == "__main__":
-    test = Lsystems.Lsystem.LSystem(rules = ["F FLFRRFLF", "L LF"], keywords = (("F", '4'), "B", "L", "R", ''))
-    print(test)
+t_new_arr = new_arr(lib.my_first_func(t_arr1, size))
+print(*t_arr1)
+print(*t_new_arr)  # <__main__.c_long_Array_3 object at 0x000002881F89AE40>
+print(t_new_arr[0])  # random number
+print(t_new_arr[1])  # 0
+print(t_new_arr[2])  # 0
